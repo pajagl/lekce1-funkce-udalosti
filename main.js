cba3883c129da54754974c147e314771f54355c4
pajagl <114282176+pajagl@users.noreply.github.com>
2023-01-25 20:10:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // tady je místo pro náš program
 
+// Změna barvy čtvereček
+function zmenaBarvy () {
+  document.querySelector(".ctverecek").style.backgroundColor = "green";
+}
+
 document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
 /**
@@ -13,9 +18,28 @@ function secti(a, b) {
   return c;
 }
 
+// výsledek po stisknutí tlačítka
+
+function zobrazVysledek() {
+  document.querySelector("#vysledek").innerHTML = secti (4, 5);
+}
+
 /**
  * Upozorní uživatele při spuštění.
  */
+
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+
+  // propsání do konzole 
+  console.log("Gratulace, právě jsi spustila tuto funkci :)");
+
+  //nahrazení textu ve čtverci
+  let squareElement = document.getElementsByClassName("ctverecek")[0]
+  squareElement.innerHTML = "Gratulace, právě si spustila tuto funkci!";
+  squareElement.style.width = "fit-content";
+  squareElement.style.backgroundColor = "grey";
 }
+
+
+
